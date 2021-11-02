e6561dabe6caad8cf841a6ab76e2ecb7e0179a40
FesalBadday <fesal.n04@hotmail.com>
2021-11-02 10:44:23 -0600

    tweaks

--- server.js
+++ server.js
@@ -8,23 +8,44 @@ const gallery = require('./gallery.js') // import gallery module
 
 // Dynamic JSON Endpoint
 app.get('/api/cars', (req, res) => {
-  res.send(gallery)
+  if (typeof gallery !== 'undefined') {
+    // Variable is an array!
+    res.send(gallery)
+  } else {
+    res.status(404)
+    res.send({ error: 'File Not Found' })
+  }
 })
 
 app.get('/api/cars/:id', (req, res) => {
-  const foundCar = gallery.find(car => Number(req.params.id) === car.id);
+  if (typeof gallery !== 'undefined') {
+    const foundCar = gallery.find(car => Number(req.params.id) === car.id);
 
-  if (!foundCar) { // send 404 if car is not found
-    res.send(['404 Not Found'])
-  } else { // else show the array
-    res.send(foundCar)
+    if (!foundCar) { // send 404 if car is not found
+      res.status(404)
+      res.send({ error: 'File Not Found' })
+    } else { // else show the array
+      res.send(foundCar)
+    }
+
+  } else {
+    res.status(404)
+    res.send({ error: 'File Not Found' })
   }
 })
 
 // Handle 404 errors with middleware
 app.use((req, res) => {
-  res.status(404)
-  res.send('404: File Not Found')
+
+  // If path starts with `/api`, send JSON 404
+  if (req.url.startsWith('/api')) {
+    res.status(404)
+    res.send({ error: 'File Not Found' })
+  } else {
+    // else send HTML 404
+    res.status(404)
+    res.send('<h1>404: File Not Found</h1>')
+  }
 });
 
 // Start server
